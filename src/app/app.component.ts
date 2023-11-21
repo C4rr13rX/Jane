77434a9ec4533e64a4ac5814e6acd4d02257df04
C4rr13rX <C4rr13rX@gmail.com>
2023-11-21 16:22:36 -0500

    update src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp, bookOutline, codeSlashOutline, colorPaletteOutline, chatBubblesOutline} from 'ionicons/icons';
+import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp, bookOutline, codeSlashOutline, colorPaletteOutline, chatbubblesOutline} from 'ionicons/icons';
 
 @Component({
   selector: 'app-root',
@@ -21,6 +21,6 @@ export class AppComponent {
   ];
   public labels = [];
   constructor() {
-    addIcons({ chatBubblesOutline, colorPaletteOutline, paperPlaneOutline, bookOutline, codeSlashOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp });
+    addIcons({ chatbubblesOutline, colorPaletteOutline, paperPlaneOutline, bookOutline, codeSlashOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp });
   }
 }
