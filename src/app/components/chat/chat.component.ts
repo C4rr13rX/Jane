8e2dab94da7591735d79c77420b83acf9f8a0fc6
C4rr13rX <C4rr13rX@gmail.com>
2023-11-22 13:32:02 -0500

    update src/app/components/chat/chat.component.ts

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss'],
+  standalone: true
 })
 export class ChatComponent  implements OnInit {
 
