a8689799e79572d542a77531d0bb5e7eecc85e91
C4rr13rX <C4rr13rX@gmail.com>
2023-11-22 12:24:09 -0500

    update src/app/folder/folder.page.ts

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,15 +1,15 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { CredentialsService } from '../services/credentials-service.service'
+
 
 @Component({
   selector: 'app-folder',
   templateUrl: './folder.page.html',
   styleUrls: ['./folder.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent],
-  providers: [CredentialsService]
+  imports: [IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent]
+  
 })
 export class FolderPage implements OnInit {
   public folder!: string;
