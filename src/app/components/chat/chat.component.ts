95497c38abfe6b2c696899a910c6a72cbaddb64b
C4rr13rX <C4rr13rX@gmail.com>
2023-11-23 02:30:44 -0500

    update src/app/components/chat/chat.component.ts

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet, IonFooter, IonTextarea } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss'],
-  standalone: true
+  standalone: true,
+  imoorts: [IonFooter, IonTextarea]
 })
 export class ChatComponent  implements OnInit {
 
