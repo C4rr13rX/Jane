027d9ff5ee7e3f37f2a7f1ebe85f8ebb1dd147ca
C4rr13rX <C4rr13rX@gmail.com>
2023-11-24 18:48:20 -0500

    update src/app/components/api-form/api-form.component.ts

--- src/app/components/api-form/api-form.component.ts
+++ src/app/components/api-form/api-form.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp, bookOutline, bookSharp, codeSlashOutline, codeSlashSharp, colorPaletteOutline, colorPaletteSharp, chatbubblesOutline, chatbubblesSharp } from 'ionicons/icons';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-api-form',
