756785b47db7af63989136944eff39e18c56e70f
C4rr13rX <C4rr13rX@gmail.com>
2023-11-24 18:03:25 -0500

    update src/app/components/chat-dialogue/chat-dialogue.component.ts

--- src/app/components/chat-dialogue/chat-dialogue.component.ts
+++ src/app/components/chat-dialogue/chat-dialogue.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { IonContent, IonButton, IonIcon, IonFooter } from '@ionic/angular/standalone'; 
 
 @Component({
   selector: 'app-chat-dialogue',
   templateUrl: './chat-dialogue.component.html',
   styleUrls: ['./chat-dialogue.component.scss'],
-  standalone: true
+  standalone: true,
+  imports: [IonContent, IonButton, IonIcon, IonFooter]
 })
 export class ChatDialogueComponent  implements OnInit {
 
