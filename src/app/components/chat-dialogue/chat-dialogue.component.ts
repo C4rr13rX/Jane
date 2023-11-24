f67de56b602ca02875989c37b42f7462849af8e1
C4rr13rX <C4rr13rX@gmail.com>
2023-11-24 18:06:09 -0500

    update src/app/components/chat-dialogue/chat-dialogue.component.ts

--- src/app/components/chat-dialogue/chat-dialogue.component.ts
+++ src/app/components/chat-dialogue/chat-dialogue.component.ts
@@ -9,13 +9,18 @@ import { IonContent, IonButton, IonIcon, IonFooter } from '@ionic/angular/standa
   imports: [IonContent, IonButton, IonIcon, IonFooter]
 })
 export class ChatDialogueComponent  implements OnInit {
+  
+  @ViewChild('dialogue', { static: false }) dialogue: IonContent;
+
+  copyText() {
+    const textToCopy = this.dialogue.el.textContent; // Get the text content from the dialogue-container
+    // Implement logic to copy the text (e.g., use Clipboard API)
+    console.log('Text copied:', textToCopy);
+  }
 
   constructor() { }
 
   ngOnInit() {}
   
-  copyText() {
-    
-  }
-
+  
 }
