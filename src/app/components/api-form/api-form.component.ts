892643aed513aeb5728f264058274090f2595ab0
C4rr13rX <C4rr13rX@gmail.com>
2023-11-24 18:47:13 -0500

    update src/app/components/api-form/api-form.component.ts

--- src/app/components/api-form/api-form.component.ts
+++ src/app/components/api-form/api-form.component.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import { IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp, bookOutline, bookSharp, codeSlashOutline, codeSlashSharp, colorPaletteOutline, colorPaletteSharp, chatbubblesOutline, chatbubblesSharp } from 'ionicons/icons';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-api-form',
   templateUrl: './api-form.component.html',
   styleUrls: ['./api-form.component.scss'],
+  standalone: true,
+  imports[FormsModule, ReactiveFormsModule]
 })
 export class ApiFormComponent  implements OnInit {
 
