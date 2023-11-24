56654d2106f7ca1f10c73a8ead30a796cfec9566
C4rr13rX <C4rr13rX@gmail.com>
2023-11-24 03:12:58 -0500

    update src/app/components/chat/chat.component.ts

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,8 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { PopoverController } from '@ionic/angular';
+import { Component, OnInit } from '@angular/core';i
 import { IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet, IonFooter, IonTextarea, IonButton} from '@ionic/angular/standalone';
-
-import { DialoguePopoverComponent } from "../../dialogue-popover/dialogue-popover.component";
 
 @Component({
   selector: 'app-chat',
@@ -13,23 +10,9 @@ import { DialoguePopoverComponent } from "../../dialogue-popover/dialogue-popove
 })
 export class ChatComponent implements OnInit {
   
-  constructor(private popoverController: PopoverController) {}
+  constructor() {}
 
   ngOnInit() {
-    
-     this.presentDialoguePopover();
-    
-  }
-  
-  async presentDialoguePopover() {
-    
-    const popover = await this.popoverController.create({
-      component: DialoguePopoverComponent, // Create a separate component for the popover content
-      cssClass: 'dialogue-popover',
-      translucent: true,
-    });
-    
-    return await popover.present();
     
   }
  
