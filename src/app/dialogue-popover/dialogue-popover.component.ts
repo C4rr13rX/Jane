2fa99be88ee1da3ecc3d438c5bffa5fa58742879
C4rr13rX <C4rr13rX@gmail.com>
2023-11-24 06:27:03 +0000

    Created DialoguePopover component.

---
+++ src/app/dialogue-popover/dialogue-popover.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialogue-popover',
+  templateUrl: './dialogue-popover.component.html',
+  styleUrls: ['./dialogue-popover.component.scss'],
+})
+export class DialoguePopoverComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
